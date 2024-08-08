e4ea1ff81a4493c3c91deeaa911a72f34085a4d4
Dan Mooar <50853302+JubJubby@users.noreply.github.com>
2024-08-08 18:13:46 -0400

    require uuid, add users array, add genres to movies objects, add endpoints for user CRUD requests

--- index.js
+++ index.js
@@ -1,52 +1,79 @@
+const bodyParser = require("body-parser");
 const express = require("express"),
     morgan = require("morgan"),
     fs = require("fs"),
-    path = require("path");
+    path = require("path"),
+    uuid = require("uuid");
 
 const app = express();
 const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {flags: "a"})
 
-//array of top ten movies
-let topMovies = [
+app.use(bodyParser.json());
+
+//array of users
+let users = [
     {
-        title: "Spaceballs",
-        director: "Mel Brooks"
+        id: 1,
+        name: "Dan",
+        favoriteMovies: []
     },
     {
-        title: "Rat Race",
-        director: "Jerry Zucker"
+        id: 2,
+        name: "Natalie",
+        favoriteMovies: []
+    },
+]
+
+//array of ten movies
+let movies = [
+    {
+        Title: "Spaceballs",
+        Director: "Mel Brooks",
+        Genre: "Comedy"
     },
     {
-        title: "Monty Python and the Holy Grail",
-        director: [
+        Title: "Rat Race",
+        Director: "Jerry Zucker",
+        Genre: "Comedy"
+    },
+    {
+        Title: "Monty Python and the Holy Grail",
+        Director: [
             "Terry Gillam", "Terry Jones"
-        ]
+        ],
+        Genre: "Comedy"
     },
     {
-        title: "Tenacious D in The Pick of Destiny",
-        director: "Liam Lynch"
+        Title: "Tenacious D in The Pick of Destiny",
+        Director: "Liam Lynch",
+        Genre: "Comedy"
     },
     {
-        title: "Donnie Darko",
-        director: "Richard Kelly"
+        Title: "Donnie Darko",
+        Director: "Richard Kelly",
+        Genre: "Horror"
     },
     {
-        title: "Interstellar",
-        director: "Christopher Nolan"
+        Title: "Interstellar",
+        Director: "Christopher Nolan",
+        Genre: "Science Fiction"
     },
     {
-        title: "Interstella 5555",
-        director: [
+        Title: "Interstella 5555",
+        Director: [
             "Leiji Matsumoto", "Kazuhisa Takenouchi", "Hirotoshi Rissen", "Daisuke Nishio"
-        ]
+        ],
+        Genre: "Anime"
     },
     {
-        title: "Bill and Ted's Excellent Adventure",
-        director: "Stephen Herek"
+        Title: "Bill and Ted's Excellent Adventure",
+        Director: "Stephen Herek",
+        Genre: "Action"
     },
     {
-        title: "This Is Spinal Tap",
-        director: "Rob Reiner"
+        Title: "This Is Spinal Tap",
+        Director: "Rob Reiner",
+        Genre: "Comedy"
     }
 ];
 
@@ -58,9 +85,77 @@ app.use((err, req, res, next) => {
     res.status(500).send("Something broke!");
 });
 
-//GET requests
+//CREATE/POST requests
+app.post("/users", (req, res) => {
+    const newUser = req.body;
+
+    if (newUser.name) {
+        newUser.id = uuid.v4();
+        users.push(newUser);
+        res.status(201).json(newUser)
+    } else {
+        res.status(400).send("users need names")
+    }
+})
+
+app.post("/users/:id/:movieTitle", (req, res) => {
+    const {id, movieTitle} = req.params;
+
+    let user = users.find(user => user.id == id);
+
+    if (user) {
+        user.favoriteMovies.push(movieTitle);
+        res.status(200).send("${movieTitle} has been added to user ${id}'s array");
+    } else {
+        res.status(400).send("no such user")
+    }
+})
+
+//DELETE requests
+app.delete("/users/:id/:movieTitle", (req, res) => {
+    const {id, movieTitle} = req.params;
+
+    let user = users.find(user => user.id == id);
+
+    if (user) {
+        user.favoriteMovies = user.favoriteMovies.filter(title => title !== movieTitle);
+        res.status(200).send("${movieTitle} has been removed from user ${id}'s array");
+    } else {
+        res.status(400).send("no such user")
+    }
+})
+
+app.delete("/users/:id", (req, res) => {
+    const {id} = req.params;
+
+    let user = users.find(user => user.id == id);
+
+    if (user) {
+        users = users.filter(user => user.id != id);
+        res.status(200).send("user ${id} has been deleted");
+    } else {
+        res.status(400).send("no such user")
+    }
+})
+
+//UPDATE/PUT requests
+app.put("/users/:id", (req, res) => {
+    const {id} = req.params;
+    const updatedUser = req.body;
+
+    let user = users.find(user => user.id == id);
+
+    if (user) {
+        user.name = updateUser.name;
+        res.status(200).json(user);
+    } else {
+        res.status(400).send("no such user")
+    }
+})
+
+//READ/GET requests
 app.get("/", (req, res) => {
-    res.send("Let's check out some movies! Head over to /movies to see my favorites.");
+    res.send("Let's check out some movies! Add '/documentation' to the end of the url for more on how to use this app.");
 });
 
 app.get("/secreturl", (req, res) => {
@@ -72,8 +167,41 @@ app.get("/documentation", (req, res) => {
 });
 
 app.get("/movies", (req, res) => {
-    res.json(topMovies);
+    res.status(200).json(movies);
 });
+
+app.get("/movies/:title", (req, res) => {
+    const {title} = req.params;
+    const movie = movies.find(movie => movie.Title === title);
+
+    if (movie) {
+        res.status(200).json(movie);
+    } else {
+        res.status(400).send("no such movie")
+    }
+})
+
+app.get("movies/genre/genreName", (req, res) => {
+    const {genreName} = req.params;
+    const genre = movies.find(movie => movie.Genre.Name === genreName).Genre;
+
+    if (genre) {
+        res.status(200).json(genre);
+    } else {
+        res.status(400).send("no such genre")
+    }
+})
+
+app.get("movies/directors/directorName", (req, res) => {
+    const {directorName} = req.params;
+    const director = movies.find(movie => movie.Director.Name === directorName).Director;
+
+    if (director) {
+        res.status(200).json(genre);
+    } else {
+        res.status(400).send("no such director")
+    }
+})
 
 //listen for requests
 app.listen(8080, () => {
