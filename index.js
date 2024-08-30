4e320a30b057e3b8415800d85a1f3dd0dfeb0102
Dan Mooar <50853302+JubJubby@users.noreply.github.com>
2024-08-30 11:49:37 -0400

    update endpoint for deleting a user by username

--- index.js
+++ index.js
@@ -145,18 +145,20 @@ app.delete("/users/:id/:movieTitle", (req, res) => {
     }
 })
 
-app.delete("/users/:id", (req, res) => {
-    const {id} = req.params;
-
-    let user = users.find(user => user.id == id);
-
-    if (user) {
-        users = users.filter(user => user.id != id);
-        res.status(200).send(`user ${id} has been deleted`);
-    } else {
-        res.status(400).send("no such user")
-    }
-})
+app.delete("/users/:Username", (req, res) => {
+    Users.findOneAndRemove({Username: req.params.Username})
+        .then((user) => {
+            if(!user) => {
+                res.status(400).send(req.params.Username + " was not found");
+            } else {
+                res.status(200).send(req.params.Username + " was deleted");
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error: " + err);
+        });
+});
 
 //UPDATE/PUT requests
 app.put("/users/:Username", (req, res) => {
