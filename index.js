9eaef24ba5e6cbc8fa859f8ce191ea887490e90f
Dan Mooar <50853302+JubJubby@users.noreply.github.com>
2024-08-30 11:06:18 -0400

    update endpoint for posting a new user

--- index.js
+++ index.js
@@ -96,15 +96,26 @@ app.use((err, req, res, next) => {
 
 //CREATE/POST requests
 app.post("/users", (req, res) => {
-    const newUser = req.body;
-
-    if (newUser.name) {
-        newUser.id = uuid.v4();
-        users.push(newUser);
-        res.status(201).json(newUser)
-    } else {
-        res.status(400).send("users need names")
-    }
+    Users.findOne({Username: req.body.Username})
+        .then((user) => {
+            if (user) {
+                return res.status(400).send(req.body.Username + "already exists")
+            } else {
+                Users.create({
+                    Username: req.body.Username,
+                    Password: req.body.Password,
+                    Email: req.body.Email,
+                    Birthday: req.body.Birthday
+                })
+                    .then((user) => {
+                        res.status(201).json(user);
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                        res.status(500).send("Error: " + error);
+                    });
+            }
+        })
 })
 
 app.post("/users/:id/:movieTitle", (req, res) => {
