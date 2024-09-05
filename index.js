f46c5a09b057576592064ff152342bbe4939eab2
Dan Mooar <50853302+JubJubby@users.noreply.github.com>
2024-09-05 09:24:07 -0400

    fix endpoint for get movie by title

--- index.js
+++ index.js
@@ -209,7 +209,7 @@ app.get("/movies", (req, res) => {
         });
 });
 
-app.get("/movies/:title", (req, res) => {
+app.get("/movies/:Title", (req, res) => {
     Movies.findOne({Title: req.params.Title})
     .then((movie) => {
         res.json(movie);
