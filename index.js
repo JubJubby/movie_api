0dc9bbb5db2f53ecae997c794776b7b4769736cb
Dan Mooar <50853302+JubJubby@users.noreply.github.com>
2024-09-30 10:04:22 -0400

    add server-side input validation for making and updating user info

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ const express = require("express"),
     uuid = require("uuid");
 const app = express();
 const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {flags: "a"})
+const cors = require("cors");
+const {check, validationResult} = require("express-validator");
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -32,27 +35,40 @@ app.use((err, req, res, next) => {
 });
 
 //CREATE/POST requests
-app.post("/users", (req, res) => {
-    Users.findOne({Username: req.body.Username})
-        .then((user) => {
-            if (user) {
-                return res.status(400).send(req.body.Username + "already exists")
-            } else {
-                Users.create({
-                    Username: req.body.Username,
-                    Password: req.body.Password,
-                    Email: req.body.Email,
-                    Birthday: req.body.Birthday
-                })
-                    .then((user) => {
-                        res.status(201).json(user);
+app.post("/users",
+    [
+        check("Username", "Username is required").isLength({min: 5}),
+        check("Username", "Username contains non-alphanumeric characters - not allowed.").isAlphanumeric(),
+        check("Password", "Password is required").not().isEmpty(),
+        check("Email", "Email does not appear to be valid").isEmail()
+    ],
+    (req, res) => {
+        let errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(422).json({errors: errors.array()});
+        }
+
+        let hashedPassword = Users.hashPassword(req.body.Password);
+        Users.findOne({Username: req.body.Username})
+            .then((user) => {
+                if (user) {
+                    return res.status(400).send(req.body.Username + "already exists")
+                } else {
+                    Users.create({
+                        Username: req.body.Username,
+                        Password: hashedPassword,
+                        Email: req.body.Email,
+                        Birthday: req.body.Birthday
                     })
-                    .catch((error) => {
-                        console.error(error);
-                        res.status(500).send("Error: " + error);
-                    });
-            }
-        })
+                        .then((user) => {
+                            res.status(201).json(user);
+                        })
+                        .catch((error) => {
+                            console.error(error);
+                            res.status(500).send("Error: " + error);
+                        });
+                }
+            })
 })
 
 app.post("/users/:Username/movies/:MovieID", passport.authenticate('jwt', { session: false }), (req, res) => {
@@ -107,28 +123,40 @@ app.delete("/users/:Username", passport.authenticate('jwt', { session: false }),
 });
 
 //UPDATE/PUT requests
-app.put("/users/:Username", passport.authenticate('jwt', { session: false }), (req, res) => {
-    if(req.user.Username !== req.params.Username){
-        return res.status(400).send('Permission denied');
-    }
-    Users.findOneAndUpdate(
-        {Username: req.params.Username},
-        {$set: {
-                Username: req.body.Username,
-                Password: req.body.Password,
-                Email: req.body.Email,
-                Birthday: req.body.Birthday
-            }
-        },
-        {new: true}) //makes sure the updated document is returned
-        .then((updatedUser) => {
-            res.json(updatedUser);
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).send("Error: " + err);
+app.put("/users/:Username", passport.authenticate('jwt', { session: false }),
+    [
+        check("Username", "Username is required").isLength({min: 5}),
+        check("Username", "Username contains non-alphanumeric characters - not allowed.").isAlphanumeric(),
+        check("Password", "Password is required").not().isEmpty(),
+        check("Email", "Email does not appear to be valid").isEmail()
+    ],
+    (req, res) => {
+        let errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({errors:errors.array()});
         }
-    )
+        
+        if(req.user.Username !== req.params.Username){
+            return res.status(400).send('Permission denied');
+        }
+        Users.findOneAndUpdate(
+            {Username: req.params.Username},
+            {$set: {
+                    Username: req.body.Username,
+                    Password: req.body.Password,
+                    Email: req.body.Email,
+                    Birthday: req.body.Birthday
+                }
+            },
+            {new: true}) //makes sure the updated document is returned
+            .then((updatedUser) => {
+                res.json(updatedUser);
+            })
+            .catch((err) => {
+                console.error(err);
+                res.status(500).send("Error: " + err);
+            }
+        )
 });
 
 //READ/GET requests
